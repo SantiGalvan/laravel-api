83cc3c32de887892df73ffb4bd1ab59093fe74ea
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-28 23:44:38 +0100

    added accessor image

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -27,5 +28,11 @@ class Project extends Model
     public function technologies()
     {
         return $this->belongsToMany(Technology::class);
+    }
+
+    // Accessor
+    public function image(): Attribute
+    {
+        return Attribute::make(fn ($value) => $value && app('request')->is('api/*') ? url('storage/' . $value) : $value);
     }
 }
