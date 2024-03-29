a4b2c9cecfb78012eab469c64702be8deb7e2d8f
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-29 12:20:28 +0100

    added type

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::with('type')->get();
 
         return response()->json($projects);
     }
