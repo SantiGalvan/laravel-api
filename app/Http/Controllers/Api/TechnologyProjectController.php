f13c7ecb5f7d6b89f043ca93c265ca6c5eb9dbe8
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-05 00:06:43 +0200

    create controller TechnologyProject

---
+++ app/Http/Controllers/Api/TechnologyProjectController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Technology;
+use Illuminate\Http\Request;
+
+class TechnologyProjectController extends Controller
+{
+}
